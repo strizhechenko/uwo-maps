cb17f4e44c0495a590654cb9586f09088c027e87
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2019-10-06 14:28:38 +0500

    Create routes.py

---
+++ utils/routes.py
@@ -0,0 +1,5 @@
+TO_INDIA = [
+    'Seville', 'Madeira', 'Las Palmas', 'Arguin', "Cabo Verde", "Sierra Leone", "Abidjan", "Saint-Georges", "Benin",
+    "Sao Tome", "Gabon", "Luanda", "Benguela", "Karibib", "Cape Town", "Natal", "Sofala", "Mozambique", "Kilwa",
+    "Zanzibar", "Malindi", "Mogadishu", "Socotra", "Jofra", "Muscat", "Diu", "Goa", "Mangalore", "Calicut", "Cochin", "Ceylon",
+    "Pondicherry", "Calcutta", "Pegu", "Banda Aceh", "Jambi", "Palembang", "Malacca", "Pattani"
