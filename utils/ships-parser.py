9fffd9a659a082025f85caafda4c74b3f09e4f32
Oleg Strizhechenko <oleg.strizhechenko@gmail.com>
2020-04-16 10:43:39 +0500

    Исправлено(парсер кораблей).

--- utils/ships-parser.py
+++ utils/ships-parser.py
@@ -1,116 +1,32 @@
-#!/usr/bin/env python3
-# coding: utf-8
+# coding=utf-8
 
-from requests import get
-import bs4
-from jinja2 import Template
-
-wiki = 'https://unchartedwaters.fandom.com'
-my_adv = 29
-my_trade = 58
-my_battle = 16
+from covid.utils import url2soup
 
 
-# def parse_args():
-#     parser = argparse.ArgumentParser()
-#     parser.add_argument('--first')
-#     parser.add_argument('--second')
-#     parser.add_argument('--route', default='to_india')
-#     return parser.parse_args()
+def ship_list():
+    """ Возвращаем список торговых кораблей"""
+    soup = url2soup("https://unchartedwaters.fandom.com/wiki/Ships/Trade_Ships")
+    ships = soup.find('table')
+    links = [ship.find('a') for ship in ships.children]
+    return [link for link in links if link != -1 and link.attrs['href'].startswith('/wiki/')]
 
 
-# def city_url(city: str) -> str:
-#     city = city.replace(" ", "_")
-#     url = 'https://unchartedwaters.fandom.com/wiki/{0}/Market'.format(city)
-#     if head(url).status_code == 404:
-#         url = 'https://unchartedwaters.fandom.com/wiki/{0}'.format(city)
-#     return url
-#
-#
-# def market_table_separate(data: str):
-#     soup = bs4.BeautifulSoup(data, "html.parser")
-#     market_table = None
-#     market_keeper = soup.find('span', id="Market_Keeper")
-#     if market_keeper:
-#         market_table = market_keeper.find_next('table')
-#     if not market_table:
-#         market_table = soup.find('table', {'style': "width: 100%;;"})
-#     if not market_table:
-#         market_table = soup.find('table', {'style': "width: 100%;"})
-#     # Some cities has list of language table first
-#     if market_table:
-#         if any('Language' in item.text for item in market_table.contents if isinstance(item, bs4.Tag)):
-#             market_table = market_table.find_next('table', {'style': "width: 100%;"})
-#     return market_table
-#
-#
-# def parse_market_table(market_table) -> set:
-#     goods = set()
-#     for link in market_table.find_all('a'):
-#         href = link.attrs.get('href', '')
-#         if not href.startswith('/wiki/'):
-#             continue
-#         if ':' in href:
-#             continue
-#         if 'Investing' in href:
-#             continue
-#         good = href.split('/')[-1]
-#         goods.add(good)
-#     return goods
-#
-#
-# def fetch_market_goods(city: str) -> set:
-#     url = city_url(city)
-#     request = get(url)
-#     market_table = market_table_separate(request.content)
-#     if not market_table:
-#         return {"Have no idea what they sale"}
-#     return parse_market_table(market_table)
-#
-#
-# def common(first, second) -> dict:
-#     first = fetch_market_goods(first)
-#     second = fetch_market_goods(second)
-#     return {
-#         "first": first,
-#         "second": second,
-#         "common": first & second
-#     }
-
-
-def ship_url_list(list_url):
-    """ Список URL кораблей """
-    data = get(list_url).text
-    soup = bs4.BeautifulSoup(data, "html.parser")
-    table = soup.find('table', attrs={'class': 'UWtable burlywood'})
-    rows = table.find_all('tr')
-    c = 0
-    for row in rows:
-        c += 1
-        cell = row.find('td')
-        if cell:
-            if cell.attrs.get('class') == 'nfo imgbox':
-                continue
-            link = cell.find('a')
-            if link and not link.attrs.get('class') and link.attrs.get('href'):
-                yield link.attrs['href']
-
-
-def parse_ship(ship_url):
-    data = get(ship_url).text
-    soup = bs4.BeautifulSoup(data, "html.parser")
-    adv = soup.find('a', attrs={'title': 'Adventure'})
-    adv, trade, battle = [int(level) for level in adv.parent.text.strip().split()]
-    if max(adv - my_adv, trade - my_trade, battle - my_battle) <= 3:
-        hold = soup.find('a', attrs={'title': 'Hold'})
-        if hold:
-            hold = int(hold.parent.parent.find_all('td', attrs={'style': "width: 50px"})[2].text.strip())
-            print(ship_url, adv, trade, battle, hold)
+def ship_process(link):
+    url = "https://unchartedwaters.fandom.com" + (link if isinstance(link, str) else link.attrs['href'])
+    ship_soup = url2soup(url)
+    ship_data = ship_soup.find('table', attrs={'class': 'BioTable'})
+    adv, trade, battle = ship_data.find('table').find_all('tr')[2].text.strip().split()
+    raw_data = ship_data.find('table', attrs={"style": not None}).find_all('tr')
+    raw_data = [item.text.strip() for item in raw_data if item.text.strip()]
+    vsail, hsail, rowing, dura = raw_data[3].split()
+    sailors, guns, cargo_hold = raw_data[6].split()
+    sailors_req = sailors.split('/')[0]
+    print("\t".join([url, adv, trade, battle, cargo_hold, sailors_req, hsail, vsail]))
 
 
 def main():
-    for ship_url in ship_url_list(wiki + '/wiki/Ships/Trade_Ships'):
-        parse_ship(wiki + ship_url)
+    for link in ship_list():
+        ship_process(link)
 
 
 if __name__ == '__main__':
